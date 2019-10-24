581390c941c98058e09f16021aac33b878ad845f
elsonlim <elsonlim7@gmail.com>
2019-10-24 11:52:58 +0800

    split less than 8 character test to be more explicit

--- passwordValidation.test.js
+++ passwordValidation.test.js
@@ -1,7 +1,12 @@
 const { passwordValidation } = require("./passwordValidation");
 describe("passwordValidation", () => {
-  it("should return true if have a minimum of 8 characters", () => {
-    expect(passwordValidation("1234567")).toBe(false);
+  it("should return true if have 8 characters or more", () => {
     expect(passwordValidation("12345678")).toBe(true);
+    expect(passwordValidation("1234567890")).toBe(true);
+  });
+
+  it("should return false if password have less than 8 characters", () => {
+    expect(passwordValidation("12345")).toBe(false);
+    expect(passwordValidation("1234567")).toBe(false);
   });
 });
