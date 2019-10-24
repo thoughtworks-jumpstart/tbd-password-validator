cc1ffec10a8d6ae14047b6a0aa52b56a642e7eab
IamCarlMichael <carl.holmberg91@gmail.com>
2019-10-24 12:37:50 +0800

    Added Password Check for numbers

--- passwordValidation.js
+++ passwordValidation.js
@@ -1,9 +1,28 @@
-module.exports.passwordValidation = password => {
-	if (password.length >= 8) {
-		const passwordArray = password.split("");
-		return passwordArray.some(item => {
-			return item === item.toLowerCase();
-        });
+const passwordValidation = password => {
+  function passwordIncludesLowerCase(password) {
+    if (password.length >= 8) {
+      const passwordArray = password.split("");
+      return passwordArray.some(item => {
+        return item === item.toLowerCase();
+      });
     }
     return false;
+  }
+
+  function passwordIncludesNumber(password) {
+    const passwordArray = password.split("");
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    return passwordArray.some(includesNum);
+    function includesNum(num) {
+      return numbers.includes(num);
+    }
+  }
+
+  return (
+    password.length >= 8 &&
+    passwordIncludesNumber(password) &&
+    passwordIncludesLowerCase(password)
+  );
 };
+
+module.exports = passwordValidation;
--- passwordValidation.test.js
+++ passwordValidation.test.js
@@ -1,4 +1,4 @@
-const { passwordValidation } = require("./passwordValidation");
+const passwordValidation = require("./passwordValidation");
 
 describe("passwordValidation", () => {
   it("should return true if have 8 characters or more", () => {
@@ -10,16 +10,23 @@ describe("passwordValidation", () => {
     expect(passwordValidation("12345")).toBe(false);
     expect(passwordValidation("1234567")).toBe(false);
   });
-	describe("should contain at least one lowercase character", () => {
-		it("should return false if there are no lowercase characters", () => {
-			expect(passwordValidation("ABCDEFGH")).toBe(false);
-		});
+  describe("should contain at least one lowercase character", () => {
+    it("should return false if there are no lowercase characters", () => {
+      expect(passwordValidation("ABCDEFGH")).toBe(false);
+    });
 
-		it("should return true if there at least one lowercase characters", () => {
-			expect(passwordValidation("aBCDE123")).toBe(true);
-		});
-		it("should return true if there at least two lowercase characters", () => {
-			expect(passwordValidation("AbcDxE123")).toBe(true);
-		});
-	});
+    it("should return true if there at least one lowercase characters", () => {
+      expect(passwordValidation("aBCDE123")).toBe(true);
+    });
+    it("should return true if there at least two lowercase characters", () => {
+      expect(passwordValidation("AbcDxE123")).toBe(true);
+    });
+  });
+
+  it("should return true if password have a numba", () => {
+    expect(passwordValidation("abcdefg1")).toBe(true);
+  });
+  it("should return true if password have a numba", () => {
+    expect(passwordValidation("abcde")).toBe(false);
+  });
 });
